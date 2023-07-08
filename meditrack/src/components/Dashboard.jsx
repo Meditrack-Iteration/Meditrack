faa7d112263106dfe3daeb2e7f46be972d11d41a
John Bilunas <johnnyb7184@gmail.com>
2023-07-08 17:52:19 -0400

    Initial Dashboard set up

--- meditrack/src/components/Dashboard.jsx
+++ meditrack/src/components/Dashboard.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 const Dashboard = props => {
+    const {username} = props;
+    const [patientsArray, setPatientsArray] = useState([]);
+
+    useEffect( () => {
+        console.log("useEffect fetch")
+        const username = localStorage.getItem('username');
+        fetch(`/dashboard/${username}`)
+        .then(data => data.json()) 
+        .then(data => {
+            setPatientsArray(data.patients);
+        })
+        // .then(data => console.log("log" ,data))
+        .catch(() => console.log("got nothing"))
+
+    }, [])
+    
+
+
     return (
-        <div>
-            Dashboard
+        <div className = 'dashboard-container'>
+            <h2>Welcome {username}</h2>
+            <button>Add Patient</button>
         </div>
     );
 };
