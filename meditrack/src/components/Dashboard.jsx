92c813808c65a42abf1ff5507d52761ce42c8fe2
Lisandro Olivares <116845904+mxlisandro@users.noreply.github.com>
2023-07-24 21:04:35 -0500

    completed merge with dev

--- meditrack/src/components/Dashboard.jsx
+++ meditrack/src/components/Dashboard.jsx
@@ -15,7 +15,6 @@ const Dashboard = props => {
         .then((data) => data.json()) 
         .then((data) => {
             console.log(data)
-            setuserName(data.firstName);
             setPatientsArray(data.patients);
         })
         .catch(() => console.log("got nothing"))
@@ -58,7 +57,7 @@ const Dashboard = props => {
         <div>
         <Navbar />
         <div className = 'dashboard-container'>
-            <h2>Welcome, {userName}!</h2>
+            <h2>Welcome, {firstName}!</h2>
             <h3 className="patients-header">Patients</h3>
             <div className="patients-container">
             {patientsArray && <PatientList className="patients-list" patients = { patientsArray } handleAddPatient={handleAddPatient}></PatientList>}
